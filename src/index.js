73a14a9b3005199b00edfe56da5fed03041c724b
Ally Juma Said <mahadsaid95@icloud.com>
2024-10-16 21:18:32 -0700

    add

--- src/index.js
+++ src/index.js
@@ -29,17 +29,17 @@ const colRef = collection(db,"books")
 
 // //get collection data 
 
-// getDocs(colRef)
-// .then((snapshot)=>{
-//     let books = []
-//     snapshot.docs.forEach((doc) => {
-//         books.push({...doc.data(), id:doc.id})
-//     })
-//     console.log(books) 
-// })
-// .catch(err => {
-//     console.log(err.message)
-// })
+getDocs(colRef)
+.then((snapshot)=>{
+    let books = []
+    snapshot.docs.forEach((doc) => {
+        books.push({...doc.data(), id:doc.id})
+    })
+    console.log(books) 
+})
+.catch(err => {
+    console.log(err.message)
+})
 
 //get collection data 
 
