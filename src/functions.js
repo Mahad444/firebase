388179dcb79d57603f3541274e4aa02c53900b30
Ally Juma Said <mahadsaid95@icloud.com>
2024-11-08 23:44:46 -0700

    path

--- src/functions.js
+++ src/functions.js
@@ -2,3 +2,7 @@
 export const add = (a, b) => a + b;
 export const subtract = (a, b) => a - b;
 export const multiply = (a, b) => a * b;
+// Path: src/index.js
+// index.js
+import { add, subtract, multiply } from './functions';
+console.log(add(1, 2)); // 3
