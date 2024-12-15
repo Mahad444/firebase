0e34fbb7081e0704251216378dd386c6522f0e14
Ally Juma Said <mahadsaid95@icloud.com>
2024-12-14 23:23:10 -0700

    firestore

---
+++ firestore.js
@@ -0,0 +1,47 @@
+// firestore is a NoSQL database that stores data in the form of collections and documents.
+
+{
+    // get collection data with where clause
+
+    const que = query(colRef, where('author', '==', 'mahad'))
+    // calling the query
+    getDocs(que)
+        .then((snapshot) => {
+            let books = []
+            snapshot.docs.forEach((doc) => {
+                books.push({ ...doc.data(), id: doc.id })
+            })
+            console.log(books)
+        })
+
+    // next we will use onSnapshot to listen to changes in the database
+
+    onSnapshot(colRef, (snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    }
+    )
+
+    // ordering data
+    onSnapshot(colRef, (snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    }
+    )
+
+    // ordering data
+    onSnapshot(colRef, (snapshot) => {
+        let books = []
+        snapshot.docs.forEach((doc) => {
+            books.push({ ...doc.data(), id: doc.id })
+        })
+        console.log(books)
+    }
+    )
+}
