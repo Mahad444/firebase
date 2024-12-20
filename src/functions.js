cf3b4168f2c9effe4247033d0ab29442aa50cdcc
Ally Juma Said <mahadsaid95@icloud.com>
2024-12-19 20:57:18 -0700

    add if

--- src/functions.js
+++ src/functions.js
@@ -8,3 +8,8 @@ import { add, subtract, multiply } from './functions';
 console.log(add(1, 2)); // 3
 console.log(subtract(3, 2)); // 1
 console.log(multiply(2, 3)); // 6
+console.log('Hello World')
+
+if (module.hot) {
+    module.hot.accept();
+}
